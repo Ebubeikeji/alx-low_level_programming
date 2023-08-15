ec3e1748bc8be17b31fbc902c7af2cb1bf06d3cd
Ebube Ikeji <ikejiebube3@gmail.com>
2023-08-15 16:15:58 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of number
+ *
+ * @n: interger
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
